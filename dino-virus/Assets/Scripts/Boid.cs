1aed8d9276385c4543e6448088cf7258e1e12a10
lucasjaenisch <lucasirmaododaniel@gmail.com>
2019-11-19 19:24:37 -0300

    seek is done trex can now chase you
    run

---
+++ dino-virus/Assets/Scripts/Boid.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boid : MonoBehaviour
+{
+    public float speed;
+    public float maxSpeed;
+    public float rotateSpeed;
+    public GameObject target;
+
+    private Rigidbody rb;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    private void Update()
+    {
+        Seek();
+    }
+
+    public void Seek()
+    {
+        Vector3 desired = new Vector3();
+
+        desired = target.transform.position - transform.position;
+        desired.Normalize();
+
+        Vector3 rotateAmount = Vector3.Cross(desired, transform.forward);
+
+        rb.angularVelocity = -rotateAmount * rotateSpeed;
+
+        rb.velocity = transform.forward * speed;
+
+        if (rb.velocity.magnitude > maxSpeed)
+        {
+            rb.velocity = rb.velocity.normalized * maxSpeed;
+        }
+
+        Debug.DrawRay(transform.position, transform.forward.normalized * 2, Color.green);
+        Debug.DrawRay(transform.position, desired.normalized * 2, Color.magenta);
+    }
+
+}
---
+++ dino-virus/Assets/Scripts/CameraControl.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+
+    public GameObject target;
+    public Transform camTransform;
+
+    private Camera cam;
+
+    public float distance = 10.0f;
+    public float currentX = 0.0f;
+    public float currentY = 0.0f;
+    public float sensitivityX = 4.0f;
+    public float sensitivityY = 1.0f;
+
+    private Vector3 offset;
+
+    public const float y_angle_min = -50.0f;
+    public const float y_angle_max = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        camTransform = transform;
+        cam = Camera.main;
+        //offset = transform.position - target.transform.position;
+    }
+
+    private void Update()
+    {
+        currentX += Input.GetAxis("Mouse X") * sensitivityX;
+        currentY += Input.GetAxis("Mouse Y") * sensitivityY;
+
+        currentY = Mathf.Clamp(currentY, y_angle_min, y_angle_max);
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 dir = new Vector3(0, 0, distance);
+        Quaternion rotation = Quaternion.Euler(currentY, currentX, 0);
+        camTransform.position = target.transform.position + rotation * dir;
+        camTransform.LookAt(target.transform.position);
+
+        //transform.position = target.transform.position + offset;
+    }
+}
